5cc032a76ce1772aecec87c62ba3b073ae326081
GZennurov <zwgybds@yahoo.com>
2020-03-07 07:52:23 +0300

    Added authentication to server

--- test.ts
+++ test.ts
@@ -3,15 +3,15 @@ import axios from "axios";
 
 const port = process.env.PORT || 4000;
 
-// axios.post(`http://localhost:${ port }/auth/signUp`, querysting.stringify({ username: 'Jora', password: '1234' }) )
-// .then((res) => {
-//     console.log(res.data);
-// });
-
-axios.post(`http://localhost:${ port }/auth/signin`,  querysting.stringify({ username: 'Jora', password: '123' }))
+axios.post(`http://localhost:${ port }/auth/signUp`, querysting.stringify({ username: 'Jora', password: '1234' }) )
 .then((res) => {
     console.log(res.data);
 });
+
+// axios.post(`http://localhost:${ port }/auth/signin`,  querysting.stringify({ username: 'Jora', password: '123' }))
+// .then((res) => {
+//     console.log(res.data);
+// });
 
 
 
